25dbd4db608780282812ad232e1bbc3378604666
AdamPongor <adam.pongor14@gmail.com>
2024-11-19 03:13:11 +0100

    imports

--- app/src/main/java/bme/aut/sza/honfoglalo/feature/join/JoinScreen.kt
+++ app/src/main/java/bme/aut/sza/honfoglalo/feature/join/JoinScreen.kt
@@ -1,6 +1,5 @@
 package bme.aut.sza.honfoglalo.feature.join
 
-import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -11,35 +10,27 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.FilledTonalButton
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.SnackbarDuration
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import bme.aut.sza.honfoglalo.R
-import bme.aut.sza.honfoglalo.data.util.getDeviceIPAddress
 import bme.aut.sza.honfoglalo.ui.theme.FlatCornerShape
 import bme.aut.sza.honfoglalo.ui.theme.Purple40
 import bme.aut.sza.honfoglalo.ui.util.UiEvent
-import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.launch
 
 
